8ef71c625ceaf377d200052b3c43d0304d2b1266
wuzeru <lswuzeru@gmail.com>
2014-05-29 17:11:01 +0800

    2.0

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,28 @@
  * 微信服务器网址接入
  +------------------------------------------------------------------------------
  */
+var crypto = require('crypto');
+var shasum = crypto.createHash('sha1');
+
+
 exports.index = function(req, res){
     var signature = req.query.signature;
     var timestamp = req.query.timestamp;
     var nonce = req.query.nonce;
 
+    var tmpStr = '';
+    tmpStr.concat("000000" + timestamp + nonce);
+    tmpStr.sort();
+
+    shasum.update(tmpStr);
+    var result = shasum.digest('hex');
+
     console.log(signature + timestamp + nonce);
 
-    res.send("false");
+    if(signature == result){
+        res.send('true');
+    }else {
+        res.send("false");
+    }
+
 };
